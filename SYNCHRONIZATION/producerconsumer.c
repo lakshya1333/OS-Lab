2377de135cfcef05e36f101f4a4b6a19b29ee931
Lakshya Jain <148108892+lakshya1333@users.noreply.github.com>
2025-02-21 07:15:43 +0530

    Update producerconsumer.c

--- SYNCHRONIZATION/producerconsumer.c
+++ SYNCHRONIZATION/producerconsumer.c
@@ -1,72 +1,69 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 5  // Buffer capacity
-#define MAX_ITEMS 10   // Number of items to produce/consume
+#define CAPACITY 5  // Buffer size
 
-int buffer[BUFFER_SIZE]; 
+int buffer[CAPACITY]; 
 int in = 0, out = 0; 
 
-
-sem_t empty; 
-sem_t full; 
+sem_t empty, full; 
 pthread_mutex_t mutex; 
 
 // Producer function
 void* producer(void* arg) {
-    for (int i = 0; i < MAX_ITEMS; i++) {
-        int item = rand() % 100;  
+    int item;
+    while (1) {
+        item = rand() % 100; 
+        sem_wait(&empty);     // Wait if buffer is full
+        pthread_mutex_lock(&mutex); // Lock critical section
 
-        sem_wait(&empty);  
-        pthread_mutex_lock(&mutex); 
-
-        
-        buffer[in] = item;
+        buffer[in] = item; 
         printf("Producer produced: %d\n", item);
-        in = (in + 1) % BUFFER_SIZE;  
+        in = (in + 1) % CAPACITY;
 
-        pthread_mutex_unlock(&mutex);  
-        sem_post(&full); 
+        pthread_mutex_unlock(&mutex); // Unlock critical section
+        sem_post(&full); // Signal that buffer has a new item
 
         sleep(1);
     }
-    return NULL;
 }
 
 // Consumer function
 void* consumer(void* arg) {
-    for (int i = 0; i < MAX_ITEMS; i++) {
-        sem_wait(&full);  
-        pthread_mutex_lock(&mutex); 
+    int item;
+    while (1) {
+        sem_wait(&full);  // Wait if buffer is empty
+        pthread_mutex_lock(&mutex); // Lock critical section
 
-      
-        int item = buffer[out];
+        item = buffer[out]; 
         printf("Consumer consumed: %d\n", item);
-        out = (out + 1) % BUFFER_SIZE;  
+        out = (out + 1) % CAPACITY;
 
-        pthread_mutex_unlock(&mutex);   
-        sem_post(&empty);  
+        pthread_mutex_unlock(&mutex); // Unlock critical section
+        sem_post(&empty); // Signal that buffer has a free space
 
-        sleep(1); 
+        sleep(2); 
     }
-    return NULL;
 }
 
 int main() {
-    pthread_t prodThread, consThread
-  
-    sem_init(&empty, 0, BUFFER_SIZE);
-    pthread_mutex_init(&mutex, NULL);  
+    pthread_t prodThread, consThread;
+
+    sem_init(&empty, 0, CAPACITY);  
+    sem_init(&full, 0, 0);         
+    pthread_mutex_init(&mutex, NULL);
+
+   
     pthread_create(&prodThread, NULL, producer, NULL);
     pthread_create(&consThread, NULL, consumer, NULL);
 
+ 
     pthread_join(prodThread, NULL);
     pthread_join(consThread, NULL);
 
-    // Destroy semaphores and mut
+    
     sem_destroy(&empty);
     sem_destroy(&full);
     pthread_mutex_destroy(&mutex);
